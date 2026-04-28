6152e8e2c2d0dd0ac7a3e12b6530a3aeb82be538
Zenith <134149806+NotZenith@users.noreply.github.com>
2026-04-28 11:49:04 +0545

    Update additionUsingOOP.cpp

--- additionUsingOOP.cpp
+++ additionUsingOOP.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
 
+class Addition {
+public:
+    int n1, n2;
+    
+    void getSum() {
+        int sum = n1 + n2;
+        int average = (n1 + n2) / 2;
+        
+        cout << "n1 = " << n1 << endl;
+        cout << "n2 = " << n2 << endl;
+        cout << "sum = " << sum << endl;
+        cout << "average = " << average << endl;
+    }
+};
+
+int main() {
+    Addition a;
+    cout << "Enter n1: ";
+    cin>>a.n1;
+    cout<<"enter n2: ";
+    cin>>a.n2;
+    
+    
+    a.getSum();
+    
+    return 0;
+}
